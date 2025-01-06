2418b84028e90aeef53a6a26185db1b5b1078514
NChanantorn <66160118@go.buu.ac.th>
2025-01-06 20:57:53 +0700

    กำหนดตำแหน่ง navbar

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
-// ควบคุมการเปิด/ปิด Hamburger Menu
 const mobileMenu = document.getElementById('mobile-menu');
 const navList = document.querySelector('.nav-list');
 
 mobileMenu.addEventListener('click', () => {
-    navList.classList.toggle('active'); // เพิ่ม/ลบคลาส active
+    navList.classList.toggle('active'); // แสดง/ซ่อนเฉพาะเมนู
 });
